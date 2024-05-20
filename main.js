5888071b5bf87c52e0ffcd16f2a906b7517f1d53
katdorneles <127132542+katdorneles@users.noreply.github.com>
2024-05-20 00:32:47 -0300

    Página web desenvolvida por KARINA DORNELES

---
+++ main.js
@@ -0,0 +1,20 @@
+let nomeUsuário = "";
+let elemento = document.querySelector("#nome-usuário");
+
+while (nomeUsuário == ""){
+    nomeUsuário = prompt("Qual o seu nome?");
+}
+
+if (nomeUsuário == null){
+    elemento.textContent = 'seja muito bem-vindo.';
+}else{
+    elemento.textContent = nomeUsuário;
+}
+
+
+
+
+
+
+
+
